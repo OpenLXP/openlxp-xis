86c1339f105002cc81fa985da35874293a902864
Jean Diouf <jdiouf@deloitte.com>
2021-02-10 18:35:46 -0500

    django project set up with docker; sample api endpoint

---
+++ app/metadata_api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+class TestObjectSerializer(serializers.Serializer):
+    """Serializes a sample JSON object"""
+    name = serializers.CharField()
+    age = serializers.IntegerField()
+    occupation = serializers.CharField()
+
+# 1. MetadataLedgerSerializer
+
+# 2. SupplementalLedgerSerializer
---
+++ app/metadata_api/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from metadata_api import serializers
+
+import logging
+import sys
+import json
+
+
+logger = logging.getLogger('dict_config_logger')
+
+class MetadataLedgerView(APIView):
+    """Test API VIew"""
+    serializer_class = serializers.TestObjectSerializer
+    # metadataSerializer_class = serializers.[serializer_name]
+    # supplementSerializer_class = serializers.[serializer_name]
+
+    def post(self, request):
+        """Takes in a JSON object and prints to the console"""
+        
+        serializer = self.serializer_class(data=request.data)
+        #metadataJSON = 
+        # supplementalJSON = 
+        # metadataSerializer = self.metadataSerializer_class(metadataJSON)
+        # supplementalSerializer = 
+            #  self.supplementSerializer_class(supplementalJSON)
+
+        # Check if metadataSerializer is valid AND supplementalSerializer is
+            # valid
+        if serializer.is_valid():
+            # name = serializer.validated_data.get('name')
+            # age = serializer.validated_data.get('age')
+            # occupation = serializer.validated_data.get('occupation')
+            logger.info(json.dumps(request.data))
+
+            # convert both serializers into 1 JSON object, and return it in
+            # the response
+            return Response(serializer)
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+            
