3bbe4d70775baed5fa96cf5105e44b8a0e2ed306
Jack <jamesharris4@deloitte.com>
2021-02-11 10:21:34 -0800

    Add initial metadata_api/models.py

--- app/metadata_api/models.py
+++ app/metadata_api/models.py
@@ -1,7 +1,45 @@
 from django.db import models
 
-# Create your models here.
 
-# 1. MetadataLedger Model
+class MetadataLedger(models.Model):
+    """Model for MetadataLedger"""
 
-# 2. SupplementalLedgerModel
+    METADATA_VALIDATION_CHOICES = [('Y', 'Yes'), ('N', 'No')]
+    RECORD_ACTIVATION_STATUS_CHOICES = [('Active', 'A'), ('Inactive', 'I')]
+    unique_record_identifier = models.CharField(max_length=50,
+                                                primary_key=True,
+                                                editable=False)
+    agent_name = models.CharField(max_length=255)
+    date_inserted = models.DateTimeField(blank=True, null=True)
+    metadata_key = models.TextField()
+    metadata_hash = models.TextField(max_length=200)
+    metadata = modles.JSONField(blank=True)
+    record_status = models.CharField(max_length=10, blank=True,
+                                     choices=RECORD_ACTIVATION_STATUS_CHOICES)
+    date_deleted = models.DateTimeField(blank=True, null=True)
+    metadata_validation_date = models.DateTimeField(blank=True, null=True)
+    metadata_validation_status = models.CharField(max_length=10, blank=True,
+                                                  choices=
+                                                  METADATA_VALIDATION_CHOICES)
+
+
+class SupplementalLedger(models.Model):
+    """Model for MetadataLedger"""
+
+    METADATA_VALIDATION_CHOICES = [('Y', 'Yes'), ('N', 'No')]
+    RECORD_ACTIVATION_STATUS_CHOICES = [('Active', 'A'), ('Inactive', 'I')]
+    unique_record_identifier = models.CharField(max_length=50,
+                                                primary_key=True,
+                                                editable=False)
+    agent_name = models.CharField(max_length=255)
+    date_inserted = models.DateTimeField(blank=True, null=True)
+    metadata_key = models.TextField()
+    metadata_hash = models.TextField(max_length=200)
+    metadata = modles.JSONField(blank=True)
+    record_status = models.CharField(max_length=10, blank=True,
+                                     choices=RECORD_ACTIVATION_STATUS_CHOICES)
+    date_deleted = models.DateTimeField(blank=True, null=True)
+    metadata_validation_date = models.DateTimeField(blank=True, null=True)
+    metadata_validation_status = models.CharField(max_length=10, blank=True,
+                                                  choices=
+                                                  METADATA_VALIDATION_CHOICES)
