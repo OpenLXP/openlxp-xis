d4f8b382248b0ba297ea728187fc7995b152200b
SHGODE <SHGODE@DELOITTE.com>
2021-02-22 16:36:39 -0500

    Adding XIA to XIS metadata_ledger using REST_API.

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -1,21 +1,15 @@
+from django.utils import timezone
 from rest_framework import serializers
-from api.models import MetadataLedger, SupplementalLedger
+from api.models import MetadataLedger
 
 
-class TestObjectSerializer(serializers.Serializer):
-    """Serializes a sample JSON object"""
-    name = serializers.CharField()
-    age = serializers.IntegerField()
-    occupation = serializers.CharField()
-
-
-class MetadataLedgerSerializer(serializers.Serializer):
+class MetadataLedgerSerializer(serializers.ModelSerializer):
     """Serializes an entry into the Metadata Ledger"""
-
     class Meta:
         model = MetadataLedger
-        fields = ('unique_record_identifier',
-                  'agent_name',
+
+        fields = ['unique_record_identifier',
+                  'provider_name',
                   'date_inserted',
                   'metadata_key',
                   'metadata_hash',
@@ -23,53 +17,35 @@ class MetadataLedgerSerializer(serializers.Serializer):
                   'record_status',
                   'date_deleted',
                   'metadata_validation_date',
-                  'metadata_validation_status')
-        extra_kwargs: {
-            'unique_record_identifier': {'max_length': 50},
-            'agent_name': {'max_length': 255},
-            'date_inserted': {'blank': True, 'null': True},
-            'metadata_hash': {'max_length': 200},
-            'metadata': {'blank': True},
-            'record_status': {'max_length': 10,
-                              'blank': True,
-                              'choices': MetadataLedger.RECORD_ACTIVATION_STATUS_CHOICES
-                              },
-            'date_deleted': {'blank': True, 'null': True},
-            'metadata_validation_date': {'blank': True, 'null': True},
-            'metadata_validation_status': {'max_length': 10,
-                                           'blank': True,
-                                           'choices': MetadataLedger.METADATA_VALIDATION_CHOICES},
-        }
-
-
-class SupplementalLedgerSerializer(serializers.Serializer):
-    """Serializes an entry into the Supplemental Ledger"""
-
-    class Meta:
-        model = SupplementalLedger
-        fields = ('unique_record_identifier',
-                  'agent_name',
-                  'date_inserted',
-                  'metadata_key',
-                  'metadata_hash',
-                  'metadata',
-                  'record_status',
-                  'date_deleted',
-                  'metadata_validation_date',
-                  'metadata_validation_status')
-        extra_kwargs: {
-            'unique_record_identifier': {'max_length': 50},
-            'agent_name': {'max_length': 255},
-            'date_inserted': {'blank': True, 'null': True},
-            'metadata_hash': {'max_length': 200},
-            'metadata': {'blank': True},
-            'record_status': {'max_length': 10,
-                              'blank': True,
-                              'choices': SupplementalLedger.RECORD_ACTIVATION_STATUS_CHOICES
-                              },
-            'date_deleted': {'blank': True, 'null': True},
-            'metadata_validation_date': {'blank': True, 'null': True},
-            'metadata_validation_status': {'max_length': 10,
-                                           'blank': True,
-                                           'choices': SupplementalLedger.METADATA_VALIDATION_CHOICES},
-        }
+                  'metadata_validation_status']
+# class SupplementalLedgerSerializer(serializers.Serializer):
+#     """Serializes an entry into the Supplemental Ledger"""
+#
+#     class Meta:
+#         model = SupplementalLedger
+#         fields = ('unique_record_identifier',
+#                   'agent_name',
+#                   'date_inserted',
+#                   'metadata_key',
+#                   'metadata_hash',
+#                   'metadata',
+#                   'record_status',
+#                   'date_deleted',
+#                   'metadata_validation_date',
+#                   'metadata_validation_status')
+#         extra_kwargs:{
+#             'unique_record_identifier': {'max_length': 50},
+#             'agent_name': {'max_length': 255},
+#             'date_inserted': {'blank': True, 'null': True},
+#             'metadata_hash': {'max_length': 200},
+#             'metadata': {'blank': True},
+#             'record_status': {'max_length': 10,
+#                               'blank': True,
+#                               'choices': SupplementalLedger.RECORD_ACTIVATION_STATUS_CHOICES
+#                               },
+#             'date_deleted': {'blank': True, 'null': True},
+#             'metadata_validation_date': {'blank': True, 'null': True},
+#             'metadata_validation_status': {'max_length': 10,
+#                                            'blank': True,
+#                                            'choices': SupplementalLedger.METADATA_VALIDATION_CHOICES}
+#         }
--- app/api/views.py
+++ app/api/views.py
@@ -1,38 +1,30 @@
+import logging
+
+from rest_framework.utils import json
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from api import serializers
-import logging
-import json
-from api.serializers import MetadataLedgerSerializer, SupplementalLedgerSerializer
+from api.models import MetadataLedger
+from api.serializers import MetadataLedgerSerializer
+
 logger = logging.getLogger('dict_config_logger')
 
 class MetadataLedgerView(APIView):
-    """Test API VIew"""
-    serializer_class = serializers.TestObjectSerializer
-    metadataSerializer_class = serializers.MetadataLedgerSerializer
-    supplementSerializer_class = serializers.SupplementalLedgerSerializer
-    def post(self, request):
-        """Takes in a JSON object and prints to the console"""
-        serializer = self.serializer_class(data=request.data)
-        metadataJSON = MetadataLedgerSerializer
-        supplementalJSON = SupplementalLedgerSerializer
-        metadataSerializer = self.metadataSerializer_class(metadataJSON)
-        supplementalSerializer = self.supplementSerializer_class(supplementalJSON)
-        # Check if metadataSerializer is valid AND supplementalSerializer is
-        # valid
+    """
+    List all snippets, or create a new snippet.
+    """
+    # def get(self, request, format=None):
+    #     metadata_ledger = MetadataLedger.objects.all()
+    #     serializer = MetadataLedgerSerializer(metadata_ledger, many=True)
+    #     return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = MetadataLedgerSerializer(data=request.data, many= True)
         if serializer.is_valid():
-            # name = serializer.validated_data.get('name')
-            # age = serializer.validated_data.get('age')
-            # occupation = serializer.validated_data.get('occupation')
-            # comment
             logger.info(json.dumps(request.data))
-            # convert both serializers into 1 JSON object, and return it in
-            # the response
-            return Response(serializer)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             logger.info(json.dumps(request.data))
-            return Response(
-                serializer.errors,
-                status=status.HTTP_400_BAD_REQUEST
-            )
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
