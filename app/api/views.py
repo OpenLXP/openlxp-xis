945a0238aac4d7354ce4ec36ea02125738fb6d57
KarenAJ <kjijo@deloitte.com>
2021-03-02 15:25:36 -0500

    Refactored code and added functionality to take in the additional case of identical records. Changed admin to access elements from XIS schema.

--- app/api/views.py
+++ app/api/views.py
@@ -1,10 +1,10 @@
 import logging
-
 from rest_framework.utils import json
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from api.serializers import MetadataLedgerSerializer
+from core.models import MetadataLedger
 
 logger = logging.getLogger('dict_config_logger')
 
@@ -13,18 +13,30 @@ class MetadataLedgerView(APIView):
     """Receive metadata_ledger data from XIA"""
 
     def post(self, request):
-        serializer = MetadataLedgerSerializer(data=request.data)
+        """POST request are handled here"""
+
+        # obtaining key value for comparison of records in metadata ledger
+        key_value = request.data.get('metadata_key', None)
+        record_in_table = None
+        if key_value is not None:
+            # Comparing metadata_key value in metadata ledger
+            # to find older instances
+            record_in_table = MetadataLedger.objects.filter(
+                metadata_key=key_value, record_status='Active').first()
+        # Assign data from request to serializer
+        serializer = MetadataLedgerSerializer(record_in_table,
+                                              data=request.data)
         logger.info("Assigned to serializer")
 
-        if serializer.is_valid():
-            # If received save record in ledger and send response of UUID &
-            # created status
-            logger.info(json.dumps(request.data))
-            serializer.save()
-            return Response(serializer.data['unique_record_identifier'],
-                            status=status.HTTP_201_CREATED)
-        else:
+        if not serializer.is_valid():
             # If not received send error and bad request status
             logger.info(json.dumps(request.data))
             return Response(serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
+
+        # If received save record in ledger and send response of UUID &
+        # created status
+        logger.info(json.dumps(request.data))
+        serializer.save()
+        return Response(serializer.data['unique_record_identifier'],
+                        status=status.HTTP_201_CREATED)
---
+++ app/core/utils/utils.py
@@ -0,0 +1,55 @@
+import logging
+import boto3
+import json
+import pandas as pd
+from django.core.management.base import BaseCommand
+from core.models import XISConfiguration
+from django.utils import timezone
+
+logger = logging.getLogger('dict_config_logger')
+
+
+def aws_get():
+    bucket_name = 'xisschema'
+    return bucket_name
+
+
+def read_json_data(file_name):
+    """setting file path for json files and ingesting as dictionary values """
+    s3 = boto3.resource('s3')
+    bucket_name = aws_get()
+    json_path = s3.Object(bucket_name, file_name)
+    json_content = json_path.get()['Body'].read().decode('utf-8')
+    data_dict = json.loads(json_content)
+    return data_dict
+
+
+def get_target_validation_schema():
+    """Retrieve target validation schema from XIA configuration """
+    logger.info("Configuration of schemas and files")
+    data = XISConfiguration.objects.first()
+    target_validation_schema = data.target_schema
+    logger.info("Reading schema for validation")
+    # Read source validation schema as dictionary
+    schema_data_dict = read_json_data(target_validation_schema)
+    return schema_data_dict
+
+
+def get_required_recommended_fields_for_target_validation():
+    """Creating list of fields which are Required & Recommended"""
+    # Call function to get target validation dictionary
+    schema_data_dict = get_target_validation_schema()
+    required_dict = {}
+    recommended_dict = {}
+    # Getting key list whose Value is Required
+    for k in schema_data_dict:
+        required_list = []
+        recommended_list = []
+        for k1, v1 in schema_data_dict[k].items():
+            if v1 == 'Required':
+                required_list.append(k1)
+            if v1 == 'Recommended':
+                recommended_list.append(k1)
+            required_dict[k] = required_list
+            recommended_dict[k] = recommended_list
+    return required_dict, recommended_dict
