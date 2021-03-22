f552ae502105b97ed76d20b678f97a62b8f63271
SHGODE <SHGODE@DELOITTE.com>
2021-03-22 18:40:42 -0400

    Adding a util file and adding es connection data into Config file

---
+++ app/api/management/utils/xse_client.py
@@ -0,0 +1,18 @@
+import logging
+import os
+import requests
+
+logger = logging.getLogger('dict_config_logger')
+
+
+def get_elasticsearch_endpoint():
+    """Setting API endpoint for XIS and XSE  communication """
+    api_es_endpoint = os.environ.get('ES_ENDPOINT')
+    return api_es_endpoint
+
+
+def get_elasticsearch_index():
+    """Setting elastic search index """
+    api_es_index = os.environ.get('ES_INDEX')
+    return api_es_index
+
