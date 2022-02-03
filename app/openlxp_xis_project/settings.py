f131a1f535eb9bb60fa94f42a87f60e7f6487784
KarenAJ <kjijo@deloitte.com>
2022-02-03 16:35:01 -0500

    Updating SQL connector

--- app/openlxp_xis_project/settings.py
+++ app/openlxp_xis_project/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'openlxp_xis_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'mysql.connector.django',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
