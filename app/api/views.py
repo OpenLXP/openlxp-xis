a921425b67ee0b2f00a2aac0402a46570897c675
JDTobin <90791836+JDTobin@users.noreply.github.com>
2022-04-26 14:47:13 -0400

    Fix for optional metadata being overwritten (#42)
    * Fix for optional metadata being overwritten
    
    * update to prefill recommeneded fields as needed

--- app/api/views.py
+++ app/api/views.py
@@ -1,7 +1,15 @@
+import copy
 import logging
 import uuid
 
 from celery.result import AsyncResult
+from core.management.utils.transform_ledgers import \
+    detach_metadata_ledger_from_supplemental_ledger
+from core.management.utils.xis_internal import update_multilevel_dict
+from core.management.utils.xss_client import \
+    get_optional_and_recommended_fields_for_validation
+from core.models import CompositeLedger, MetadataLedger
+from core.tasks import xis_workflow
 from django.http import JsonResponse
 # from core.management.commands.load_metadata_into_neo4j import \
 #     Command as load_metadata_into_neo4j
@@ -19,12 +27,6 @@ from api.management.utils.api_helper_functions import (add_metadata_ledger,
 from api.serializers import (CompositeLedgerSerializer,
                              MetadataLedgerSerializer,
                              SupplementalLedgerSerializer)
-from core.management.utils.transform_ledgers import \
-    detach_metadata_ledger_from_supplemental_ledger
-from core.management.utils.xis_internal import update_multilevel_dict
-from core.management.utils.xss_client import get_optional_fields_for_validation
-from core.models import CompositeLedger, MetadataLedger
-from core.tasks import xis_workflow
 
 logger = logging.getLogger('dict_config_logger')
 
@@ -132,12 +134,16 @@ class MetaDataView(APIView):
         """This method defines the API's to save data to the
         metadata ledger in the XIS"""
 
-        # Add optional fields to the metadata
-        optional_fields = get_optional_fields_for_validation()
+        # Add optional/recommended fields to the metadata
+        extra_fields = get_optional_and_recommended_fields_for_validation()
         metadata = request.data['metadata']
-        for field in optional_fields:
-            if field not in metadata:
-                path = field.split('.')
+        for field in extra_fields:
+            meta_path = copy.deepcopy(metadata)
+            path = field.split('.')
+            try:
+                for step in path:
+                    meta_path = meta_path[step]
+            except Exception:
                 update_multilevel_dict(metadata, path, None)
         request.data['metadata'] = metadata
 
