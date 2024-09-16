6e6d2c20ab349c66bdc9667b7edc210730ce34c6
Simeranjeet Sandhu <simeransandhu@deloitte.com>
2024-09-16 07:40:23 -0700

    Updating README/Deployment

--- start-app.sh
+++ start-app.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 # start-server.sh
 
+cd /tmp/openlxp-xis/app
 python manage.py waitdb 
 python manage.py migrate  
 python manage.py createcachetable 
 python manage.py loaddata admin_theme_data.json 
-cd /tmp/app/ 
+python manage.py collectstatic --no-input
+cd /tmp/
 pwd 
 ./start-server.sh
