7d71c4a03f153e88ddbf9c932bca776664290903
JDTobin <90791836+JDTobin@users.noreply.github.com>
2025-02-05 09:54:46 -0500

    fix regex_check (#96)

--- app/core/models.py
+++ app/core/models.py
@@ -6,14 +6,17 @@ from django.db import models
 from django.forms import ValidationError
 from django.urls import reverse
 
-regex_check = (r'(?!(\A( \x09\x0A\x0D\x20-\x7E # ASCII '
-               r'| \xC2-\xDF # non-overlong 2-byte '
-               r'| \xE0\xA0-\xBF # excluding overlongs '
-               r'| \xE1-\xEC\xEE\xEF{2} # straight 3-byte '
-               r'| \xED\x80-\x9F # excluding surrogates '
-               r'| \xF0\x90-\xBF{2} # planes 1-3 '
-               r'| \xF1-\xF3{3} # planes 4-15 '
-               r'| \xF4\x80-\x8F{2} # plane 16 )*\Z))')
+regex_check = (
+    r'^('  # start at start
+    r'[\x09\x0A\x0D\x20-\x7E]'  # ASCII
+    r'|[\xC2-\xDF]'  # non-overlong 2-byte
+    r'|[\xE0\xA0-\xBF]'  # excluding overlongs
+    r'|[\xE1-\xEC\xEE\xEF]{2}'  # straight 3-byte
+    r'|[\xED\x80-\x9F]'  # excluding surrogates
+    r'|[\xF0\x90-\xBF]{2}'  # planes 1-3
+    r'|[\xF1-\xF3]{3}'  # planes 4-15
+    r'|[\xF4\x80-\x8F]{2}'  # plane 16
+    r')*$')  # match all until end
 
 
 class XISConfiguration(models.Model):
